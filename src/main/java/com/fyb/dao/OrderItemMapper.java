c2e302a0a6cf5e9ecb359a8d6f47580259c62940
bo <389616868@qq.com>
2018-01-27 00:35:38 +0800

    Initial commit

---
+++ src/main/java/com/fyb/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.fyb.dao;
+
+import com.fyb.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
